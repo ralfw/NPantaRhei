bdfb29bcb99e5fc6dbbf86d29985b41c92d67cbc
slieser <stefan@lieser-online.de>
2012-06-22 20:49:50 +0200

    Throttle added to patterns

---
+++ source/npantarhei.runtime/npantarhei.runtime/patterns/Throttle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Timers;
+using npantarhei.runtime.contract;
+
+namespace npantarhei.runtime.patterns
+{
+    public class Throttle : AOperation
+    {
+        private readonly int periodInMilliseconds;
+        private Timer timer;
+        private IMessage message;
+
+        public Throttle(string name, int periodInMilliseconds = 500)
+            : base(name) {
+            this.periodInMilliseconds = periodInMilliseconds;
+        }
+
+        protected override void Process(IMessage input, Action<IMessage> continueWith, Action<FlowRuntimeException> unhandledException) {
+            message = input;
+            if (timer == null) {
+                timer = new Timer {
+                    Interval = periodInMilliseconds
+                };
+                timer.Elapsed +=
+                    delegate {
+                        timer.Dispose();
+                        timer = null;
+                        continueWith(message);
+                    };
+                timer.Start();
+            }
+            else {
+                timer.Stop();
+                timer.Start();
+            }
+        }
+    }
+}
