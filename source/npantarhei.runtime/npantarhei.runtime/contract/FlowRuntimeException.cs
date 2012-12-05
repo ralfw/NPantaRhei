44b49e985919b785c59de4e0192e6a4c1bff34ee
Ralf Westphal <info@ralfw.de>
2012-12-05 10:21:03 +0100

    Removed serializability from exceptions
    Exceptions should not be serializable, because their data might not be
    serializable, plus the context data does not make sense in another
    process/flow.

--- source/npantarhei.runtime/npantarhei.runtime/contract/FlowRuntimeException.cs
+++ source/npantarhei.runtime/npantarhei.runtime/contract/FlowRuntimeException.cs
@@ -2,7 +2,6 @@ using System;
 
 namespace npantarhei.runtime.contract
 {
-    [Serializable]
     public class FlowRuntimeException : ApplicationException
     {
         public FlowRuntimeException(Exception exception, IMessage context) : this("Unhandled exception during operation execution.", exception, context) {}
@@ -14,7 +13,6 @@ namespace npantarhei.runtime.contract
         public IMessage Context { get; private set; }
     }
 
-    [Serializable]
     public class UnhandledFlowRuntimeException : FlowRuntimeException
     {
         public UnhandledFlowRuntimeException(FlowRuntimeException exception) : base("Unhandled exception during operation execution! Missing exception event handler or causality.", exception.InnerException, exception.Context) {}
