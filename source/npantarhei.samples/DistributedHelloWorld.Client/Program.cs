153c46c813b3e461080a5ab06146a203a3725ec3
Ralf Westphal <info@ralfw.de>
2012-12-18 10:33:17 +0100

    Added sample to show distribution of flows

---
+++ source/npantarhei.samples/DistributedHelloWorld.Client/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using npantarhei.distribution.wcf;
+using npantarhei.runtime;
+
+namespace DistributedHelloWorld.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var portnumber = 8100 + DateTime.Now.Second;
+
+            var config = new FlowRuntimeConfiguration()
+                                .AddEventBasedComponent("ui", new UI()) 
+                                .AddOperation(new WcfStandInOperation("proxy", "localhost:"+portnumber, "localhost:8000"))
+                                .AddStreamsFrom("DistributedHelloWorld.Client.root.flow", 
+                                                Assembly.GetExecutingAssembly());
+
+            using(var fr = new FlowRuntime(config))
+            {
+                Console.WriteLine("Enter names: ");
+                fr.Process(".run");
+                fr.WaitForResult();
+            }
+        }
+    }
+
+    class UI
+    {
+        public void Wait_for_names()
+        {
+            while (true)
+            {
+                var name = Console.ReadLine();
+                if (name != "")
+                    Name(name);
+                else
+                {
+                    Exit();
+                    return;
+                }
+            }
+        }
+
+        public event Action<string> Name;
+        public event Action Exit;
+
+        public void Display_greeting(string greeting)
+        {
+            Console.WriteLine(greeting);
+        }
+    }
+}
---
+++ source/npantarhei.samples/DistributedHelloWorld.Server/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using npantarhei.distribution.wcf;
+using npantarhei.runtime;
+
+namespace DistributedHelloWorld.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new FlowRuntimeConfiguration()
+                                .AddFunc<string, string>("greet", Greet)
+                                .AddStream(".@greet", "greet")
+                                .AddStream("greet", ".@greet");
+            using(var fr = new FlowRuntime(config))
+            using(new WcfOperationHost(fr, "localhost:8000"))
+            {
+                Console.WriteLine("Waiting for names...");
+                Console.ReadLine();
+            }
+        }
+
+
+        static string Greet(string name)
+        {
+            Console.WriteLine("  greet {0}", name);
+            return "Hello, " + name;
+        }
+    }
+}
