c417bd8e8c4adf20ad977ed45d04239a7cae885e
Denis Kuniß <kuniss@grammarcraft.de>
2013-10-27 18:52:12 +0100

    This test also fails from time to time at the last assertion which checks that thread used at the asynchonization of message processing for both functions are not intersect. This seems to be caused by a different thread scheduling strategy of mono which makes this visible. However, the problem should also exist on CLR but is hard to be reproduced. At first it was assumed that Thread.GetHashCode call is the reason as MSDN states that Thread.GetHashCode is not garanteed to be unique over the time. There it is recommended to use ManagedThreadId instead. However, this does not solve the problem. The problem comes from the fact that both MakeSerial calls utilize the same thread pool (according to Ralfs article http://www.dotnetpro.de/articles/onlinearticle4233.aspx about MakeAsync which should be similar for MakeSerial). So in fact the last assertion do not match the assumption of using different thread pools. This assumption could be simply made valid by passing different names to both MakeSerial calls which will use different thread pools for both functions ensuring that the last assertion did not fail.

--- source/npantarhei.runtime/npantarhei.runtime.tests/integration/test_Serialize.cs
+++ source/npantarhei.runtime/npantarhei.runtime.tests/integration/test_Serialize.cs
@@ -26,22 +26,22 @@ namespace npantarhei.runtime.tests.integration
                                                     {
                                                         lock (asyncThreadIds0)
                                                         {
-                                                            Console.WriteLine("serNop0: {0} on {1}", _, Thread.CurrentThread.GetHashCode());
-                                                            asyncThreadIds0.Add(Thread.CurrentThread.GetHashCode());
+                                                            Console.WriteLine("serNop0: {0} on {1}", _, Thread.CurrentThread.ManagedThreadId);
+                                                            asyncThreadIds0.Add(Thread.CurrentThread.ManagedThreadId);
                                                         }
                                                         return _;
-                                                    }).MakeSerial();
+                                                    }).MakeSerial("serNop0");
 
             var asyncThreadIds1 = new List<long>();
             frc.AddFunc<string, string>("serNop1", _ =>
                                                     {
                                                         lock (asyncThreadIds1)
                                                         {
-                                                            Console.WriteLine("serNop1: {0} on {1}", _, Thread.CurrentThread.GetHashCode());
-                                                            asyncThreadIds1.Add(Thread.CurrentThread.GetHashCode());
+                                                            Console.WriteLine("serNop1: {0} on {1}", _, Thread.CurrentThread.ManagedThreadId);
+                                                            asyncThreadIds1.Add(Thread.CurrentThread.ManagedThreadId);
                                                         }
                                                         return _;
-                                                    }).MakeSerial();
+                                                    }).MakeSerial("serNop1");
 
             using (var sut = new FlowRuntime(frc, new Schedule_for_async_breadthfirst_processing()))
             {
@@ -53,8 +53,8 @@ namespace npantarhei.runtime.tests.integration
                                   {
                                       lock (results)
                                       {
-                                          Console.WriteLine("res {0} on {1}", _.Data, Thread.CurrentThread.GetHashCode());
-                                          runtimeThreadId = Thread.CurrentThread.GetHashCode();
+                                          Console.WriteLine("res {0} on {1}", _.Data, Thread.CurrentThread.ManagedThreadId);
+                                          runtimeThreadId = Thread.CurrentThread.ManagedThreadId;
                                           results.Add(_);
                                           if (results.Count == N) are.Set();
                                       }
